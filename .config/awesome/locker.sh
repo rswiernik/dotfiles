5b10798e77408a202f0ca3dd09725879839a03cc
Reed Swiernik <reed.swiernik@gmail.com>
2018-11-27 22:58:40 -0800

    Add awesome to configs

---
+++ .config/awesome/locker.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+xautolock -exit
+
+# I think the exit takes a small amount of time to complete,
+# so if we just attempt to exec right away, we're trying to
+# influence an exiting xautolock and we don't get a running
+# copy... So we sleep for a second so we can laucnh a new one!
+sleep 1
+
+exec xautolock \
+    -locker "slock" \
+    -time 5 \
+    -corners "+000" \
+    -cornerdelay 1 \
+    -cornerredelay 1 \
+    -cornersize 10 \
+    -detectsleep
