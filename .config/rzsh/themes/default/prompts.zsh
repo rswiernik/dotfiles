0f982dee3be4b9c8a0e6375a1206dbc8a0f0d725
Reed Swiernik <reed.swiernik@gmail.com>
2018-12-14 21:15:56 -0800

    Add hashed hostname colors

--- .config/rzsh/themes/default/prompts.zsh
+++ .config/rzsh/themes/default/prompts.zsh
@@ -103,11 +103,7 @@ function get_user_machine {
 
     if [[ -n $SSH_CLIENT ]] || [[ -n $SSH_TTY ]] || [[ -n $SSH_AUTH_SOCK ]]; then
         # echo "%n@%m"
-<<<<<<< HEAD
         echo "[%{$fg[${CACHED_HOST_COLOR}]%}%m%{$reset_color%}]"
-=======
-        echo "[%{$fg[yellow]%}%m%{$reset_color%}]"
->>>>>>> Fix prompt styling
     fi
 }
 
