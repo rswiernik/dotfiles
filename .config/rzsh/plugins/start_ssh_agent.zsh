7a1fd6710431926dde8b79d85de9bb84fa9686e3
Reed Swiernik <reed.swiernik@gmail.com>
2020-02-20 12:35:28 -0800

    Fix error in ssh agent detection logic

--- .config/rzsh/plugins/start_ssh_agent.zsh
+++ .config/rzsh/plugins/start_ssh_agent.zsh
@@ -15,16 +15,16 @@ function start_new_ssh_agent {
 }
 
 function get_ssh_agent_status {
-    echo $(ps -f -p "${SSH_AGENT_PID}" | grep "ssh-agent$")
+    echo "$(ps -f -p "${SSH_AGENT_PID}" | grep "ssh-agent$")"
 }
 
 
 # If we dont have any agent information, attempt to start one
-if [[ -z get_ssh_agent_status ]]; then
+if [[ -z $(get_ssh_agent_status) ]]; then
     # Attempt to prime SSH env variables if we have previously set up the rzsh ssh env
     if [ -f "${RZSH_SSH_ENV}" ]; then
         . "${RZSH_SSH_ENV}" > /dev/null
     fi
     # If we still dont have a running agent, start one
-    [[ -z get_ssh_agent_status ]] && start_new_ssh_agent
+    [[ -z $(get_ssh_agent_status) ]] && start_new_ssh_agent
 fi
